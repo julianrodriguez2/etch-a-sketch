ae074ea7a903136c0c4b2f95d5a4a214373e823c
Julian Rodriguez <jrodriguezlee2@gmail.com>
2023-09-12 23:03:11 -0700

    Added a drag and mouseover functionality

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
 const gridContainer = document.getElementById("grid-container");
 const squareButton = document.getElementById('square-button');
 
-function createGrid(number){
+let mouseDown = false;
+
+// We use these events on the document to ensure that mouseDown status is accurate
+document.addEventListener('mousedown', () => { mouseDown = true; });
+document.addEventListener('mouseup', () => { mouseDown = false; });
+
+function createGrid(number) {
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild);
     }
 
     gridContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${number}, 1fr)`;
-    for (let i = 0; i < number * number; i++){
+
+    for (let i = 0; i < number * number; i++) {
         const gridSquare = document.createElement("div");
         gridSquare.classList.add("grid");
 
-        gridSquare.addEventListener("mouseover", () => {
-            gridSquare.style.backgroundColor = "black";
+        gridSquare.addEventListener('mouseenter', function(e) {
+            if (mouseDown) changeColor(e);
+        });
+        gridSquare.addEventListener('mousedown', function(e) {
+            e.preventDefault();  // Prevent the default drag-and-drop behavior
+            changeColor(e);
         });
 
         gridContainer.appendChild(gridSquare);
     }
 }
 
+function changeColor(e) {
+    e.target.style.backgroundColor = "black";
+}
+
 squareButton.addEventListener('click', function() {
-    let gridSize = prompt("Please enter the grid size in pixels (Less than 100):");
-    if (gridSize === null || gridSize > 100){
+    let gridSize = parseInt(prompt("Please enter the grid size in pixels (Less than 100):"), 10);
+    if (isNaN(gridSize) || gridSize === null || gridSize > 100) {
         alert("Grid size not changed due to invalid input.");
         return;
     }
